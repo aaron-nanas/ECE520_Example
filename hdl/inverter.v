5559badcd67662829d803d8ed54b69ddfee84637
aaron-nanas <ananas2516@gmail.com>
2026-08-24 18:28:09 -0700

    Created experimental version of inverter

--- hdl/inverter.v
+++ hdl/inverter.v
@@ -1,12 +1,18 @@
 
 module inverter
 (
-    input in,
-    output out
+    input in_1,
+    input in_2,
+    output reg out_1,
+    output reg out_2
 );
 
 always @(posedge clk) begin
-    out <= ~in;
+    out_1 <= ~in_1;
+end
+
+always @(posedge clk) begin
+    out_2 <= ~in_2;
 end
 
 endmodule
